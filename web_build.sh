93c6a7b7ab21f773ee456a8199e0ec73064f7591
Fedor <unrealfff@gmail.com>
2026-06-28 21:21:41 +0200

    Remove hardcoded path to emsdk in build script

--- web_build.sh
+++ web_build.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-EMROOT=/home/fedor/ExternalRepos/emsdk/upstream/emscripten
 
-./build_from_scratch.sh -DCMAKE_BUILD_TYPE=Release -DENABLE_SANITIZER_ADDRESS=False -DCMAKE_TOOLCHAIN_FILE=$EMROOT/cmake/Modules/Platform/Emscripten.cmake -DWEB_BUILD="/home/fedor/ExternalRepos/emsdk/upstream/emscripten" $@
+if [ -z "${EMSDK:-}" ]; then
+  printf 'Error: Environmental variable EMSDK is not set, do not know how to build for web\n' >&2
+  exit 1
+fi
+
+EMROOT="${EMSDK}/upstream/emscripten"
+
+./build_from_scratch.sh -DCMAKE_BUILD_TYPE=Release -DENABLE_SANITIZER_ADDRESS=False -DCMAKE_TOOLCHAIN_FILE=$EMROOT/cmake/Modules/Platform/Emscripten.cmake -DWEB_BUILD="${EMROOT}" $@
 
